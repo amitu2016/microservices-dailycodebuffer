d0d5c1fdb4158435a5e209b1963e40f01f407787
amitu <amitu@cmj-madhuri.cdacmumbai.in>
2021-02-26 19:33:31 +0530

    Test commit

--- src/main/java/com/amitu/department/service/DepartmentService.java
+++ src/main/java/com/amitu/department/service/DepartmentService.java
@@ -1,5 +1,12 @@
 package com.amitu.department.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.amitu.department.repository.DepartmentRepository;
+
 public class DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
 
 }
