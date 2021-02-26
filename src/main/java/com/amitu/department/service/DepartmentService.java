820c418b8b983ddfdfce4576497955b6fa201aef
amitu <amitu@cmj-madhuri.cdacmumbai.in>
2021-02-26 19:50:22 +0530

    TEST

--- src/main/java/com/amitu/department/service/DepartmentService.java
+++ src/main/java/com/amitu/department/service/DepartmentService.java
@@ -1,12 +1,19 @@
 package com.amitu.department.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.amitu.department.entity.Department;
 import com.amitu.department.repository.DepartmentRepository;
 
+@Service
 public class DepartmentService {
 	
 	@Autowired
 	private DepartmentRepository departmentRepository;
+
+	public Department saveDepartment(Department department) {
+		return departmentRepository.save(department);
+	}
 
 }
